9bf96cf3d874e1c782f7a25cf7c56296d12c806e
ryankwei <rkwei@usc.edu>
2020-01-17 12:56:13 -0800

    uibuild

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+morgan.token('person', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+let phoneList = [
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2},
+  { name: "Dan Abramov", number: "12-43-234345", id: 3},
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4}
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(phoneList)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${phoneList.length} people</p><p>${Date().toString()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const person = phoneList.find(person => person.id === Number(request.params.id))
+  if(person)
+    response.json(person)
+  else
+    response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phoneList = phoneList.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  if(!body.name || !body.number) {
+    return response.status(400).json({ error: 'content missing'})
+  }
+
+  if(phoneList.some(person => person.name === body.name)) {
+    return response.status(400).json({ error: 'name must be unique'})
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.random()*1000
+  }
+
+  phoneList = phoneList.concat(person)
+
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, ()=> {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ precache-manifest.767d6c80c9c60d1ef51da5c2005cf991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed0602df23785601e9f19ffc463174f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26e4b6219cbadefefec",
+    "url": "/static/css/main.4e29f17c.chunk.css"
+  },
+  {
+    "revision": "b225591c6073f507e9a3",
+    "url": "/static/js/2.025bce77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.025bce77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e26e4b6219cbadefefec",
+    "url": "/static/js/main.41695c69.chunk.js"
+  },
+  {
+    "revision": "be7945bcf2803547815a",
+    "url": "/static/js/runtime-main.b3a9c755.js"
+  }
+]);
