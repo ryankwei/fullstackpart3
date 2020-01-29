1e2b30ad5eb3a93fbd4d9ad9f60084e102a136e6
ryankwei <rkwei@usc.edu>
2020-01-29 10:12:46 -0800

    uibuild

---
+++ 1-29.py
@@ -0,0 +1,18 @@
+even_numbers = []
+for i in range(10):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+num_list = [1,3,5,6,7,8,10,13]
+mult_by_three = []
+for i in num_list:
+    mult_by_three.append(i*3)
+
+square_numbers = []
+for i in range(10):
+    square_numbers.append(i*i)
+
+nums = [1,1,3,3,2,2,4,5,5,7,1,1]
+nums_set = {}
+for i in nums:
+    nums_set.add(i)
